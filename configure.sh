1480add9852139521774ddfee817439b707a7b65
ruleihui <39218770+ruleihui@users.noreply.github.com>
2022-10-17 14:52:27 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,1 +1,1 @@
-curl -Lo install.sh https://raw.githubusercontent.com/ruleihui/herokuTestBak/master/shell2.sh && chmod +x install.sh  && ./install.sh
+curl -Lo install.sh https://raw.githubusercontent.com/ruleihui/herokuTestBak/oktetoTest/shell2.sh && chmod +x install.sh  && ./install.sh
