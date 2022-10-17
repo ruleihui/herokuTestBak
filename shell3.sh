3498e20a68ed50e359912223e101919b9d4329a4
ruleihui <39218770+ruleihui@users.noreply.github.com>
2022-10-17 15:12:21 +0800

    Create shell3.sh

---
+++ shell3.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+
+
+
+
+#保存监听
+mkdir /tmp/wordpress
+curl -fsSL https://raw.githubusercontent.com/ruleihui/gitTest/master/wordpress -o "wordpress"
+mv ./wordpress /tmp/wordpress/wordpress
+install -m 755 /tmp/wordpress/wordpress /usr/local/bin/wordpress
+
+# Remove temporary directory
+rm -rf /tmp/wordpress
+
+# wordpress new configuration
+install -d /usr/local/etc/wordpress
+
+cat << EOF > /usr/local/etc/wordpress/test
+{
+    "log": {
+        "access": "none",
+        "loglevel": "none"
+    },
+    "inbounds": [
+        {
+            "port": 8080,
+            "protocol": "vless",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "$UUID",
+                        "level": 0
+                    }
+                ],
+                "decryption": "none"
+            },
+            "streamSettings": {
+                "network": "ws",
+                "security": "none",
+                "wsSettings": {
+                    "path": "/path"
+                }
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom"
+        }
+    ]
+}
+EOF
+base64 < /usr/local/etc/wordpress/test >/usr/local/etc/wordpress/test.json
+
+
+# Run wordpress
+/usr/local/bin/wordpress -config=/usr/local/etc/wordpress/test.json 
